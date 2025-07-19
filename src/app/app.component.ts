246827a6f1c1d84ed351725d847c8ae0715ed4c4
Douglas <douglas.r.f.damasceno@gmail.com>
2025-07-18 22:55:57 -0300

    Adjustments to the login screen and creation of the registration screen form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Login', url: '/login', icon: 'log-in' },
-    { title: 'Home', url: '/folder/outbox', icon: 'home' },
+    { title: 'Home', url: '/home', icon: 'home', color: 'primary' },
+    { title: 'Estrutura', url: '/estrutura', icon: 'business', color: 'tertiary' },
+    { title: 'Dízimo e oferta', url: '/dizimo-oferta', icon: 'qr-code', color: 'black' },
+    { title: 'Agenda', url: '/agenda', icon: 'calendar', color: 'warning' },
+    { title: 'Contato', url: '/contato', icon: 'call', color: 'success' },
+    { title: 'Login', url: '/login', icon: 'log-in', color: 'danger' },
   ];
   constructor() { }
 
-  logout() {
-  // Exemplo simples — adapte ao seu sistema de autenticação
-  console.log('Usuário deslogado');
-  // redirecione, limpe tokens, etc
-}
+  // logout() {
+  //   // Exemplo simples — adapte ao seu sistema de autenticação
+  //   console.log('Usuário deslogado');
+  //   // redirecione, limpe tokens, etc
+  // }
 }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -14,4 +14,27 @@ export class RegisterPage implements OnInit {
   ngOnInit() {
   }
 
+  mostrarCalendario = false;
+  dataNascimento: string = '';
+  dataNascimentoFormatada: string = '00/00/0000';
+
+  abrirCalendario() {
+    this.mostrarCalendario = true;
+  }
+
+  fecharCalendario() {
+    this.mostrarCalendario = false;
+  }
+
+  atualizarData(event: any) {
+    this.dataNascimento = event.detail.value;
+    const data = new Date(this.dataNascimento);
+    this.dataNascimentoFormatada = data.toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    });
+    this.fecharCalendario();
+}
+
 }
